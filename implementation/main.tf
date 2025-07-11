be13f14907ffe7b6a6a718563163b501c3f2541a
alokaneunice <alokaneunice@outlook.com>
2025-07-11 15:12:48 +0100

    good commit

--- implementation/main.tf
+++ implementation/main.tf
@@ -1,9 +1,9 @@
-resource "aws_instance" "publi-server" {
+resource "aws_instance" "public-server" {
 
   ami                         = var.ami
   instance_type               = var.instance_type
-  key_name                    = "devopskey2"
-  subnet_id                   = aws_subnet.myapp-subnet-1.id
+  key_name                    = "neem"
+  subnet_id                   = aws_subnet.public-subnet-1.id
   vpc_security_group_ids      = [aws_default_security_group.default-sg.id]
   availability_zone           = var.avail_zone
   associate_public_ip_address = true
@@ -16,8 +16,8 @@ resource "aws_instance" "private-server" {
 
   ami                         = var.ami
   instance_type               = var.instance_type
-  key_name                    = "devopskey2"
-  subnet_id                   = aws_subnet.myapp-subnet-2.id
+  key_name                    = "neem"
+  subnet_id                   = aws_subnet.private-subnet-2.id
   vpc_security_group_ids      = [aws_default_security_group.default-sg.id]
   availability_zone           = var.avail_zone
   associate_public_ip_address = true
